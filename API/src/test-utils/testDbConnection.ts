f87d8c6cead465f4cf3b072426b36d0cc1828cde
Harry Prior <harry.prior@gfk.com>
2021-05-13 12:29:12 +0100

    Add test utils

---
+++ API/src/test-utils/testDbConnection.ts
@@ -0,0 +1,14 @@
+import { createConnection } from 'typeorm';
+
+export const testDbConnection = () => {
+  return createConnection({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'dvdrental',
+    synchronize: false,
+    entities: [__dirname + '/../entities/*.*'],
+  });
+};
