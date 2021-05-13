6f474309239495ccf4e492032207ca7b0dea9137
Harry Prior <harry.prior@gfk.com>
2021-05-13 12:29:20 +0100

    add main index file

---
+++ API/src/index.ts
@@ -0,0 +1,14 @@
+import getDbConnection from './config/dbConnection';
+import createExpressApp from './config/createExpressApp';
+import { Connection } from 'typeorm';
+
+const main = async () => {
+  const connection: Connection = await getDbConnection();
+  console.log(`Connected to ${connection.driver.database}`);
+
+  const app = await createExpressApp();
+
+  app.listen(4000, () => console.log(`Server ready at http://localhost:${process.env.PORT}/`));
+};
+
+main().catch(err => console.error(err));
