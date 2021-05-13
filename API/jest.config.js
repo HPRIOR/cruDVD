de01308983e0a94c869c90cc922fef870fd4d868
Harry Prior <harry.prior@gfk.com>
2021-05-13 12:29:33 +0100

    add jest and tsconfig files

---
+++ API/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  testPathIgnorePatterns: ["/node_modules"],
+  setupFiles: ['dotenv/config'],
+  verbose: true,
+  // collectCoverage: true,
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 25,
+  //     functions: 25,
+  //     lines: 25,
+  //     statements: 25,
+  //   },
+  // },
+  collectCoverageFrom: ["**/*.{ts,js}", "!**/node_modules/**", "!**/vendor/**"],
+  coveragePathIgnorePatterns: ["/node_modules"],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+};
